3e2980c2d979d19a4206cb5f04597b408cde0b22
Televnoy <vtelevnoy@gmail.com>
2026-02-16 22:20:57 +0500

    sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'braid-tone-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
