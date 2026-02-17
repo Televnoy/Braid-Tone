ebff2b3c9130b867903c24b6419587206789efa7
Televnoy <vtelevnoy@gmail.com>
2026-02-17 22:18:39 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,20 @@
-const CACHE_NAME = 'braid-tone-v1';
-const ASSETS = [
+const CACHE_NAME = 'braid-tone-v2';
+const ASSETS_TO_CACHE = [
   'index.html',
   'manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/lucide@latest',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js'
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
@@ -22,3 +25,4 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
